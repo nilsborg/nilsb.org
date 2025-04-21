ec544cb744228bb57669040dcdcf4e911a6129e6
Nils Borgböhmer <nils@dinghy.studio>
2025-04-21 21:12:01 +0200

    Refactor Footer and Header components; update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "nilsb.org";
+export const SITE_DESCRIPTION =
+  "Hi my name is Nils and this is my website. Sharing photos, bookmarks and blogs here and there.";
